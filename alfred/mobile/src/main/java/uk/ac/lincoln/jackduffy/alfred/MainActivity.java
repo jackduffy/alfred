d52877bae4650c2b310d2f2dcfbe6aa061080759
Jack <jack_duffy@outlook.com>
2017-02-02 17:42:22 +0000

    ^

--- alfred/mobile/src/main/java/uk/ac/lincoln/jackduffy/alfred/MainActivity.java
+++ alfred/mobile/src/main/java/uk/ac/lincoln/jackduffy/alfred/MainActivity.java
@@ -1,22 +1,57 @@
 package uk.ac.lincoln.jackduffy.alfred;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import com.google.android.gms.common.data.FreezableUtils;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.PutDataRequest;
+import com.google.android.gms.wearable.Wearable;
+import com.google.android.gms.wearable.WearableListenerService;
+
+
+import java.util.Date;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener
+{
+    GoogleApiClient googleClient;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Build a new GoogleApiClient
+        googleClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +62,101 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    // Connect to the data layer when the Activity starts
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        googleClient.connect();
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle)
+    {
+//        String WEARABLE_DATA_PATH = "/wearable_data";
+//
+//        // Create a DataMap object and send it to the data layer
+//        DataMap dataMap = new DataMap();
+//        dataMap.putString("THIS WAS SENT FROM THE PHONE!", "1");
+//
+//
+////        dataMap.putLong("time", new Date().getTime());
+////        dataMap.putString("hole", "1");
+////        dataMap.putString("front", "250");
+////        dataMap.putString("middle", "260");
+////        dataMap.putString("back", "270");
+//
+//        //Requires a new thread to avoid blocking the UI
+//        new SendToDataLayerThread(WEARABLE_DATA_PATH, dataMap).start();
+    }
+
+    // Disconnect from the data layer when the Activity stops
+    @Override
+    protected void onStop()
+    {
+        if (null != googleClient && googleClient.isConnected()) {
+            googleClient.disconnect();
+        }
+        super.onStop();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {}
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {}
+
+    class SendToDataLayerThread extends Thread {
+        String path;
+        DataMap dataMap;
+
+        // Constructor for sending data objects to the data layer
+        SendToDataLayerThread(String p, DataMap data) {
+            path = p;
+            dataMap = data;
+        }
+
+        public void run() {
+            // Construct a DataRequest and send over the data layer
+            PutDataMapRequest putDMR = PutDataMapRequest.create(path);
+            putDMR.getDataMap().putAll(dataMap);
+            PutDataRequest request = putDMR.asPutDataRequest();
+            DataApi.DataItemResult result = Wearable.DataApi.putDataItem(googleClient, request).await();
+            if (result.getStatus().isSuccess()) {
+                Log.v("myTag", "DataMap: " + dataMap + " sent successfully to data layer ");
+            }
+            else {
+                // Log an error
+                Log.v("myTag", "ERROR: failed to send DataMap to data layer");
+            }
+        }
+    }
+
+    public void sendTestMessage(View view)
+    {
+        String WEARABLE_DATA_PATH = "/wearable_data";
+        DataMap dataMap = new DataMap();
+        dataMap.putString("THIS WAS SENT FROM THE PHONE!", "1");
+        new SendToDataLayerThread(WEARABLE_DATA_PATH, dataMap).start();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
